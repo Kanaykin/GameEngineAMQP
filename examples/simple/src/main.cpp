d075a0e0fb5ca5edc29d7d58dd0ca362ac8e7922
Sergey Kanaykin <s.kanaykin@game-insight.com>
2022-03-17 08:24:38 +0300

    refs #3 qpid proton example with two differents threads

--- examples/simple/src/main.cpp
+++ examples/simple/src/main.cpp
@@ -13,12 +13,15 @@
 #include <proton/sender_options.hpp>
 #include <proton/source_options.hpp>
 #include <proton/tracker.hpp>
+#include <proton/delivery.hpp>
+#include <proton/receiver_options.hpp>
 
 #include <iostream>
 #include <map>
 #include <string>
 #include <sstream>
 #include <cctype>
+#include <thread>
 
 static const int fixed_channel_id = 1;
 static const char test_queue_name[] = "test_queue";
@@ -92,6 +95,83 @@ class server : public proton::messaging_handler {
     }
 };
 
+using proton::receiver_options;
+using proton::source_options;
+
+class client : public proton::messaging_handler {
+  private:
+    std::string url;
+    std::vector<std::string> requests;
+    proton::sender sender;
+    proton::receiver receiver;
+
+  public:
+    client(const std::string &u, const std::vector<std::string>& r) : url(u), requests(r) {}
+
+    void on_container_start(proton::container &c) override {
+        sender = c.open_sender(url);
+        // Create a receiver requesting a dynamically created queue
+        // for the message source.
+        receiver_options opts = receiver_options().source(source_options().dynamic(true));
+        receiver = sender.connection().open_receiver("", opts);
+    }
+
+    void send_request() {
+        proton::message req;
+        req.body(requests.front());
+        req.reply_to(receiver.source().address());
+        sender.send(req);
+    }
+
+    void on_receiver_open(proton::receiver &) override {
+        send_request();
+    }
+
+    void on_message(proton::delivery &d, proton::message &response) override {
+        if (requests.empty()) return; // Spurious extra message!
+
+        std::cout << requests.front() << " => " << response.body() << std::endl;
+        requests.erase(requests.begin());
+
+        if (!requests.empty()) {
+            send_request();
+        } else {
+            d.connection().close();
+        }
+    }
+};
+void thread_function()
+{
+    try
+    {
+        server hw("amqp://127.0.0.1:5672/examples");
+        proton::container(hw).run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+void thread_function2()
+{
+    try {
+        std::vector<std::string> requests;
+        requests.push_back("Twas brillig, and the slithy toves");
+        requests.push_back("Did gire and gymble in the wabe.");
+        requests.push_back("All mimsy were the borogroves,");
+        requests.push_back("And the mome raths outgrabe.");
+
+        client c("amqp://127.0.0.1:5672/examples", requests);
+        proton::container(c).run();
+
+        return 0;
+//    } catch (const example::bad_option& e)
+//    {
+//        std::cout << opts << std::endl << e.what() << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
 int main()
 {
 //    amqp_connection_state_t connection_state = amqp_new_connection();
@@ -127,13 +207,11 @@ int main()
 ////        publish();
 //    }
 
-    try
-    {
-        server hw("amqp://127.0.0.1:5672/examples");
-        proton::container(hw).run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
+    std::thread t(&thread_function);   // t starts running
+    
+    std::thread t2(&thread_function2);   // t starts running
+    
+    t.join();   // main thread waits for the thread t to finish
     
     return 0;
 }

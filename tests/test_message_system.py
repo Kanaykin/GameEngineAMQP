354e68c61acc9cbb1eb1ac4c9a69a47e38fd2809
Sergey Kanaykin <s.kanaykin@game-insight.com>
2022-04-22 08:21:48 +0300

    #8 clean puthon test

--- tests/test_message_system.py
+++ tests/test_message_system.py
@@ -5,6 +5,25 @@ from subprocess import Popen
 
 BIN_DIR = '../build/bin/'
 
+def multy_proc_tests(cmds, checkResultFunc):
+	print("multy_proc_tests ")
+	procs = [ subprocess.Popen(i, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) for i in cmds ]
+	for p in procs:
+		p.wait()
+
+	full_out = ""
+	for p in procs:
+		while True:
+			line = p.stdout.readline()
+			if not line:
+				break
+			#the real code does filtering here
+			# print(str(line))
+			full_out = full_out + str(line)
+
+	checkResultFunc(full_out)
+	
+
 class TestWeekendDays(unittest.TestCase):
 
 	def test_broker(self):
@@ -29,55 +48,21 @@ class TestWeekendDays(unittest.TestCase):
 		print("test_peer_to_peer_client_server ")
 
 		cmds = [BIN_DIR + "peer-to-peer-server", BIN_DIR + "peer-to-peer-client"]
+		checkResultFunc = lambda outstr: (
+			self.assertFalse(outstr.find("ProtonConsumer::on_message [consumer]") == -1))
 
-		procs = [ subprocess.Popen(i, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) for i in cmds ]
-		for p in procs:
-			p.wait()
+		multy_proc_tests(cmds, checkResultFunc)
 
-		full_out = ""
-		for p in procs:
-			print("str(out) =========== ")
-			out, err = p.communicate()
-			print(str(out))
-			full_out = full_out + str(out)
-		
-		self.assertFalse(full_out.find("ProtonConsumer::on_message [consumer]") == -1)
 
 	def test_broker_client_server(self):
 		print("test_broker_client_server ")
 
 		cmds = [ BIN_DIR + "broker_exchange", BIN_DIR + "broker_producer", BIN_DIR + "broker_consumer"]
-		procs = [ subprocess.Popen(i, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) for i in cmds ]
-		for p in procs:
-			p.wait()
-
-		full_out = ""
-		for p in procs:
-			# print("str(out) =========== ")
-			# out, err = p.communicate()
-			# print(str(out))
-			while True:
-				line = p.stdout.readline()
-				if not line:
-					break
-				#the real code does filtering here
-				print(str(line))
-				full_out = full_out + str(line)
-
-		self.assertFalse(full_out.find("ProtonConsumer::on_message [customer1]") == -1)
-		self.assertFalse(full_out.find("ProtonConsumer::on_message [customer2]") == -1)
-
-		# while True:
-		# 	line = full_out.readline()
-		# 	if not line:
-		# 		break
-		# 	#the real code does filtering here
-		# 	print("test:" + line.rstrip())
-
-	# 	result = subprocess.run(command)
-		# self.assertFalse(str(result.stdout).find("ProtonConsumer::on_message [consumer]") == -1)
-
-		# self.assertEqual(result.returncode, 0)
+		
+		checkResultFunc = lambda outstr: (self.assertFalse(outstr.find("ProtonConsumer::on_message [customer1]") == -1), 
+		self.assertFalse(outstr.find("ProtonConsumer::on_message [customer2]") == -1))
+		
+		multy_proc_tests(cmds, checkResultFunc)
 
 
 if __name__ == '__main__':

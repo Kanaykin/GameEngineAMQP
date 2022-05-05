0be203c1425cca1ffd9400b8b7554cfca0b45219
Sergey Kanaykin <s.kanaykin@game-insight.com>
2022-05-06 00:19:48 +0300

    #11 log4cpp added

---
+++ cmake/log4cpp.cmake
@@ -0,0 +1,4 @@
+cmake_minimum_required(VERSION 3.10)
+
+include_directories(${CMAKE_ROOT_PATH}/dependence/log4cpp/include)
+#set(LIBRARIES ${LIBRARIES} log4cpp)
--- make/make_3rdparty_libs.sh
+++ make/make_3rdparty_libs.sh
@@ -8,50 +8,63 @@ CURRENT_DIR=${PWD}
 ROOT_DIR=${CURRENT_DIR}/..
 echo "Script executed from: ${CURRENT_DIR}"
 
-# rabbitmq-c
+# # rabbitmq-c
+# #------------------
+# echo "make rabbitmq-c ... "
+# cd ../engine/3rdparty/rabbitmq-c
+
+# mkdir build 
+# cd build
+
+# cmake -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/rabbitmq-c ..
+# cmake --build . --target install
+
+# # amqpcpp
+# #------------------
+# echo "make amqpcpp ... "
+# cd ${ROOT_DIR}/engine/3rdparty/amqpcpp
+
+# mkdir build 
+# cd build
+
+# cmake -DCMAKE_DEPENDENCE_PATH=${ROOT_DIR}/dependence ..
+# cmake --build . --target install
+
+# # cmake .. -DMY_VARIABLE=0
+
+# # qpid-proton
+# #------------------
+# cd ${ROOT_DIR}/engine/3rdparty/qpid-proton
+
+# mkdir build 
+# cd build
+
+# cmake .. -DBUILD_EXAMPLES=0 -DBUILD_STATIC_LIBS=1 -DPROACTOR=libuv -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/qpid-proton
+# cmake --build . --target install
+
+# # logging-log4cxx
+# #------------------
+# brew install apr
+# brew install apr-util
+
+# cd ${ROOT_DIR}/engine/3rdparty/logging-log4cxx
+
+# mkdir build 
+# cd build
+
+# # -DBUILD_SHARED_LIBS=0
+# cmake ..  -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/logging-log4cxx
+# cmake --build . --target install
+
+# log4cpp
 #------------------
-echo "make rabbitmq-c ... "
-cd ../engine/3rdparty/rabbitmq-c
 
-mkdir build 
-cd build
-
-cmake -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/rabbitmq-c ..
-cmake --build . --target install
-
-# amqpcpp
-#------------------
-echo "make amqpcpp ... "
-cd ${ROOT_DIR}/engine/3rdparty/amqpcpp
-
-mkdir build 
-cd build
-
-cmake -DCMAKE_DEPENDENCE_PATH=${ROOT_DIR}/dependence ..
-cmake --build . --target install
-
-# cmake .. -DMY_VARIABLE=0
-
-# qpid-proton
-#------------------
-cd ${ROOT_DIR}/engine/3rdparty/qpid-proton
-
-mkdir build 
-cd build
-
-cmake .. -DBUILD_EXAMPLES=0 -DBUILD_STATIC_LIBS=1 -DPROACTOR=libuv -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/qpid-proton
-cmake --build . --target install
-
-# logging-log4cxx
-#------------------
-brew install apr
-brew install apr-util
-
-cd ${ROOT_DIR}/engine/3rdparty/logging-log4cxx
+cd ${ROOT_DIR}/engine/3rdparty/log4cpp
+./configure
 
 mkdir build 
 cd build
 
 # -DBUILD_SHARED_LIBS=0
-cmake ..  -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/logging-log4cxx
+cmake ..  -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/dependence/log4cpp
 cmake --build . --target install

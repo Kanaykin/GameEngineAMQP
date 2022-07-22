bb6bd480391e0861b926f9363a2e03691a1ec83f
Kanaykin <kanaykin.sergey@gmail.com>
2022-07-22 08:38:11 +0300

    fix log4cpp compilation

---
+++ engine/3rdparty/log4cpp/build/CMakeFiles/log4cpp.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/AbortAppender.cpp" "CMakeFiles/log4cpp.dir/src/AbortAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/AbortAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Appender.cpp" "CMakeFiles/log4cpp.dir/src/Appender.o" "gcc" "CMakeFiles/log4cpp.dir/src/Appender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/AppenderSkeleton.cpp" "CMakeFiles/log4cpp.dir/src/AppenderSkeleton.o" "gcc" "CMakeFiles/log4cpp.dir/src/AppenderSkeleton.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/AppendersFactory.cpp" "CMakeFiles/log4cpp.dir/src/AppendersFactory.o" "gcc" "CMakeFiles/log4cpp.dir/src/AppendersFactory.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/BasicConfigurator.cpp" "CMakeFiles/log4cpp.dir/src/BasicConfigurator.o" "gcc" "CMakeFiles/log4cpp.dir/src/BasicConfigurator.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/BasicLayout.cpp" "CMakeFiles/log4cpp.dir/src/BasicLayout.o" "gcc" "CMakeFiles/log4cpp.dir/src/BasicLayout.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/BufferingAppender.cpp" "CMakeFiles/log4cpp.dir/src/BufferingAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/BufferingAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Category.cpp" "CMakeFiles/log4cpp.dir/src/Category.o" "gcc" "CMakeFiles/log4cpp.dir/src/Category.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/CategoryStream.cpp" "CMakeFiles/log4cpp.dir/src/CategoryStream.o" "gcc" "CMakeFiles/log4cpp.dir/src/CategoryStream.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Configurator.cpp" "CMakeFiles/log4cpp.dir/src/Configurator.o" "gcc" "CMakeFiles/log4cpp.dir/src/Configurator.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/DailyRollingFileAppender.cpp" "CMakeFiles/log4cpp.dir/src/DailyRollingFileAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/DailyRollingFileAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/DllMain.cpp" "CMakeFiles/log4cpp.dir/src/DllMain.o" "gcc" "CMakeFiles/log4cpp.dir/src/DllMain.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/DummyThreads.cpp" "CMakeFiles/log4cpp.dir/src/DummyThreads.o" "gcc" "CMakeFiles/log4cpp.dir/src/DummyThreads.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/FactoryParams.cpp" "CMakeFiles/log4cpp.dir/src/FactoryParams.o" "gcc" "CMakeFiles/log4cpp.dir/src/FactoryParams.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/FileAppender.cpp" "CMakeFiles/log4cpp.dir/src/FileAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/FileAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Filter.cpp" "CMakeFiles/log4cpp.dir/src/Filter.o" "gcc" "CMakeFiles/log4cpp.dir/src/Filter.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/FixedContextCategory.cpp" "CMakeFiles/log4cpp.dir/src/FixedContextCategory.o" "gcc" "CMakeFiles/log4cpp.dir/src/FixedContextCategory.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/HierarchyMaintainer.cpp" "CMakeFiles/log4cpp.dir/src/HierarchyMaintainer.o" "gcc" "CMakeFiles/log4cpp.dir/src/HierarchyMaintainer.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/IdsaAppender.cpp" "CMakeFiles/log4cpp.dir/src/IdsaAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/IdsaAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/LayoutAppender.cpp" "CMakeFiles/log4cpp.dir/src/LayoutAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/LayoutAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/LayoutsFactory.cpp" "CMakeFiles/log4cpp.dir/src/LayoutsFactory.o" "gcc" "CMakeFiles/log4cpp.dir/src/LayoutsFactory.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/LevelEvaluator.cpp" "CMakeFiles/log4cpp.dir/src/LevelEvaluator.o" "gcc" "CMakeFiles/log4cpp.dir/src/LevelEvaluator.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Localtime.cpp" "CMakeFiles/log4cpp.dir/src/Localtime.o" "gcc" "CMakeFiles/log4cpp.dir/src/Localtime.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/LoggingEvent.cpp" "CMakeFiles/log4cpp.dir/src/LoggingEvent.o" "gcc" "CMakeFiles/log4cpp.dir/src/LoggingEvent.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/MSThreads.cpp" "CMakeFiles/log4cpp.dir/src/MSThreads.o" "gcc" "CMakeFiles/log4cpp.dir/src/MSThreads.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/NDC.cpp" "CMakeFiles/log4cpp.dir/src/NDC.o" "gcc" "CMakeFiles/log4cpp.dir/src/NDC.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/NTEventLogAppender.cpp" "CMakeFiles/log4cpp.dir/src/NTEventLogAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/NTEventLogAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/OmniThreads.cpp" "CMakeFiles/log4cpp.dir/src/OmniThreads.o" "gcc" "CMakeFiles/log4cpp.dir/src/OmniThreads.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/OstreamAppender.cpp" "CMakeFiles/log4cpp.dir/src/OstreamAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/OstreamAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PThreads.cpp" "CMakeFiles/log4cpp.dir/src/PThreads.o" "gcc" "CMakeFiles/log4cpp.dir/src/PThreads.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PassThroughLayout.cpp" "CMakeFiles/log4cpp.dir/src/PassThroughLayout.o" "gcc" "CMakeFiles/log4cpp.dir/src/PassThroughLayout.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PatternLayout.cpp" "CMakeFiles/log4cpp.dir/src/PatternLayout.o" "gcc" "CMakeFiles/log4cpp.dir/src/PatternLayout.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PortabilityImpl.cpp" "CMakeFiles/log4cpp.dir/src/PortabilityImpl.o" "gcc" "CMakeFiles/log4cpp.dir/src/PortabilityImpl.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Priority.cpp" "CMakeFiles/log4cpp.dir/src/Priority.o" "gcc" "CMakeFiles/log4cpp.dir/src/Priority.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Properties.cpp" "CMakeFiles/log4cpp.dir/src/Properties.o" "gcc" "CMakeFiles/log4cpp.dir/src/Properties.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PropertyConfigurator.cpp" "CMakeFiles/log4cpp.dir/src/PropertyConfigurator.o" "gcc" "CMakeFiles/log4cpp.dir/src/PropertyConfigurator.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/PropertyConfiguratorImpl.cpp" "CMakeFiles/log4cpp.dir/src/PropertyConfiguratorImpl.o" "gcc" "CMakeFiles/log4cpp.dir/src/PropertyConfiguratorImpl.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/RemoteSyslogAppender.cpp" "CMakeFiles/log4cpp.dir/src/RemoteSyslogAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/RemoteSyslogAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/RollingFileAppender.cpp" "CMakeFiles/log4cpp.dir/src/RollingFileAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/RollingFileAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/SimpleConfigurator.cpp" "CMakeFiles/log4cpp.dir/src/SimpleConfigurator.o" "gcc" "CMakeFiles/log4cpp.dir/src/SimpleConfigurator.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/SimpleLayout.cpp" "CMakeFiles/log4cpp.dir/src/SimpleLayout.o" "gcc" "CMakeFiles/log4cpp.dir/src/SimpleLayout.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/StringQueueAppender.cpp" "CMakeFiles/log4cpp.dir/src/StringQueueAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/StringQueueAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/StringUtil.cpp" "CMakeFiles/log4cpp.dir/src/StringUtil.o" "gcc" "CMakeFiles/log4cpp.dir/src/StringUtil.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/SyslogAppender.cpp" "CMakeFiles/log4cpp.dir/src/SyslogAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/SyslogAppender.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/TimeStamp.cpp" "CMakeFiles/log4cpp.dir/src/TimeStamp.o" "gcc" "CMakeFiles/log4cpp.dir/src/TimeStamp.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/TriggeringEventEvaluatorFactory.cpp" "CMakeFiles/log4cpp.dir/src/TriggeringEventEvaluatorFactory.o" "gcc" "CMakeFiles/log4cpp.dir/src/TriggeringEventEvaluatorFactory.o.d"
+  "/Users/s.kanaykin/Documents/Home/Engines/GameEngineAMQP/engine/3rdparty/log4cpp/src/Win32DebugAppender.cpp" "CMakeFiles/log4cpp.dir/src/Win32DebugAppender.o" "gcc" "CMakeFiles/log4cpp.dir/src/Win32DebugAppender.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
